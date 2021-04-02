6fb8ba21e7f7af822876d531c47a1d178c6e093e
Meherin Akter <meherinakter13@gmail.com>
2021-04-02 21:04:54 +0600

    delete smoothly

--- src/components/ManageProduct/ManageProduct.js
+++ src/components/ManageProduct/ManageProduct.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import ManageFurniture from '../ManageFurniture/ManageFurniture';
+
 
 const ManageProduct = () => {
     const [furnitures ,setFurnitures] = useState([])
@@ -8,6 +8,28 @@ const ManageProduct = () => {
         .then(res =>res.json())
         .then(data => setFurnitures(data))
     },[])
+    const deleteProduct  = () =>{
+        fetch(`https://gentle-woodland-19462.herokuapp.com/furnitures`)
+        .then(res =>res.json())
+        .then(data => setFurnitures(data))
+       }
+        
+        const handleDelete = (id) => {
+          fetch(`https://gentle-woodland-19462.herokuapp.com/deleteProduct/${id}`, {
+              method : "DELETE"
+          })
+          .then(res => res.json())
+          .then(data=>{
+              if(data){
+                  alert('Product Deleted')
+                  deleteProduct();
+                  // location.reload(false);
+              }
+              // history.push(0);
+              
+              // furniture.target.node.style.display = 'none';
+          })
+      }
     return (
         <div className=" container">
             <table class="table table-primary col-md-12 col-sm-12">
@@ -23,7 +45,20 @@ const ManageProduct = () => {
             </table>
          <div className="row">
             {
-                furnitures.map(furniture=><div className="col-md-12 col-sm-12"><ManageFurniture furniture={furniture}> </ManageFurniture></div>)
+                furnitures.map(furniture=><div className="col-md-12 col-sm-12">
+                    <table class="table table-primary">
+                        <tbody>
+                            <tr>
+                            <td className="w-25">{furniture.name}</td>
+                            <td className="w-25">{furniture.description}</td>
+                            <td className="w-25">{furniture.price}</td>
+                            <td className="w-25"> <img src={furniture.imageURL} style={{width: "100px",height:"100px"}} alt=""/></td>
+                            <td className="w-25"><button className ="btn btn-danger mt-3" onClick = {()=>handleDelete(furniture._id)}>Delete</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    {/* <ManageFurniture furniture={furniture}> </ManageFurniture> */}
+                    </div>)
             }
         </div>
         </div>
